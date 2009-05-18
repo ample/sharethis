a1209b6429a43758769e9a37e7933b26ae898af6
Bobby Uhlenbrock <bobby@thinkbarefoot.com>
2009-05-18 17:40:16 -0400

    A few changes for the new version

--- lib/sharethis_fu_helper.rb
+++ lib/sharethis_fu_helper.rb
@@ -5,17 +5,19 @@ module SharethisFuHelper
   SharethisJSParams = [ :title, :url, :content, :icon, :summary, :updated, :published ]
   
   def sharethis(options = {})
-    entry = options.collect { |k,v| "#{k}:\"#{v}\"" if SharethisJSParams.include?(k) }.compact.join(';')
+    entry = options.collect { |k,v| "#{k}:\"#{v}\"" if SharethisJSParams.include?(k) }.compact.join(",\n")
     button = options[:button] || true
     <<-END
       <script type=\"text/javascript\">
-        SHARETHIS.addEntry({#{entry}},{button:#{button}});
+        SHARETHIS.addEntry({
+          #{entry}
+        },{button:#{button}});
       </script>
     END
   end
   
   def include_sharethis_js
-    %{<script type="text/javascript" src="http://w.sharethis.com/widget/?#{@sharethis_widget_config}"></script>}
+    %{<script type="text/javascript" src="http://w.sharethis.com/button/sharethis.js##{@sharethis_widget_config}"></script>}
   end
   
   def include_sharethis_if_needed
